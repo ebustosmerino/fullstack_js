40f949deec07c65b9939552d7c68844868d22856
ebustos <ebustos@deseis.cl>
2020-02-22 11:29:37 -0300

    definicion de api rest v0

--- node/app.js
+++ node/app.js
@@ -8,4 +8,20 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+app.get('/products', (req, res) => {
+    res.send('Listado de productos');
+});
+
+app.get('/products/:id', (req, res) => {
+    res.send('Obtener producto especifico');
+});
+
+app.get('/products/:id/entregar', (req, res) => {
+    res.send('Marcar como entregado un producto especifico');
+});
+
+app.post('/products', (req, res) => {
+    res.send('Creacion de producto');
+});
+
 module.exports = app;
