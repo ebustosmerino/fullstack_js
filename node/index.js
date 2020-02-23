27c2d58e6d778032c275c9c656d7e875bb6cad89
ebustos <ebustos@deseis.cl>
2020-02-23 11:27:10 -0300

    nodemon

--- node/index.js
+++ node/index.js
@@ -21,7 +21,7 @@ mongoose.connect('mongodb://localhost:27017/mongo_db0', {
 
         // CREAR EL SERVIDOR WEB CON NODEJS
         app.listen(port, () => {
-            console.log("servidor corriendo en http://localhost:3800");
+            console.log("servidor corriendo en http://localhost:" + port);
         });
     })
     .catch(err => console.log(err));
