0ed5d401f2b8b2218e9023cfe52fa914e3a1f900
ebustos <ebustos@deseis.cl>
2020-02-23 22:39:58 -0300

    add controller to saveUser, use bodyParser for convert data in format Json

--- node/controllers/user.js
+++ node/controllers/user.js
@@ -1,11 +1,54 @@
 'use strict';
 
-function test(req, res) {
-    res.status(200).send({
-        message: 'Accion del controlador de usuarios'
-    });
+var User = require('../models/user');
+var bcrypt = require('bcrypt-nodejs');
+
+function saveUser(req, res) {
+    var user = new User();
+
+    var params = req.body;
+
+    console.log(params);
+
+    user.name = params.name;
+    user.email = params.email;
+    user.role = 'ROLE_USER';
+    user.image = 'null';
+
+    if (params.password) {
+        bcrypt.hash(params.password, null, null, function (err, hash) {
+            user.password = hash;
+            if (user.name != null && user.email != null) {
+                user.save((err, userStored) => {
+                    if (err) {
+                        res.status(500).send({
+                            message: 'Error al guardar el usuario'
+                        });
+                    } else {
+                        if (!userStored) {
+                            res.status(404).send({
+                                message: 'No se ha registrado el usuario'
+                            });
+                        } else {
+                            res.status(200).send({
+                                user: userStored
+                            });
+                        }
+                    }
+                });
+            } else {
+                res.status(200).send({
+                    message: 'Introduce todos los campos'
+                });
+            }
+        });
+    } else {
+        res.status(200).send({
+            message: 'Introduce password'
+        });
+    }
 }
 
 module.exports = {
-    test
+    saveUser
 }
